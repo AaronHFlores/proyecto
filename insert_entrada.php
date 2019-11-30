89dadac895a25d84df5c63ed7a03c3440a558803
Aaron Abraham Hernandez Flores <aaron.hflores@alumnos.udg.mx>
2019-11-30 03:49:24 -0600

    a

---
+++ insert_entrada.php
@@ -0,0 +1,11 @@
+<?php
+include 'index.php';
+
+$valor = pg_query_escape_string($con, $_GET['valor']);
+
+$querycont = pg_query($conexion, "INSERT INTO entradas(puerto) VALUES ('".$valor."');");
+//pg_query($conexion, "INSERT INTO ventas(gpurificada,galcalina) VALUES (1,1);");
+
+pg_close($conexion);
+
+ ?>
