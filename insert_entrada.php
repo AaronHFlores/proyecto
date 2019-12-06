c4790c1b0d0242865aa88b0a72767207fec14aac
Lorena <lorenaestrada554@gmail.com>
2019-12-05 21:52:18 -0600

    a

--- insert_entrada.php
+++ insert_entrada.php
@@ -2,8 +2,9 @@
 include 'index.php';
 
 $valor = $_POST['valor'];
+$fecha = $_POST['fecha'];
 
-$querycont = pg_query($conexion, "INSERT INTO entradas(puerto) VALUES (".$valor.");");
+$querycont = pg_query($conexion, "INSERT INTO entradas(puerto,fecha) VALUES (".$valor.",'".$fecha."');");
 //pg_query($conexion, "INSERT INTO ventas(gpurificada,galcalina) VALUES (1,1);");
 
 pg_close($conexion);
